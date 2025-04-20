daea97f9b56050c7a0e65a869606533a2ca8eaeb
umutbyr <byrumut@outlook.com>
2025-04-20 18:54:21 +0300

    fix: ES-Lint issues has been fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Page() {
   return (
     <main className="flex flex-col items-center justify-center h-screen gap-4">
       <h1>Welcome to Quiz App</h1>
-      <Link href="/" className="bg-amber-400 rounded-4xl p-4 text-2xl">
-        Start today's quiz!
+      <Link href="/quiz/1" className="bg-amber-400 rounded-4xl p-4 text-2xl">
+        <p>Start today{"'"}s quiz!</p>
       </Link>
     </main>
   );
